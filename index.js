d2d0f4e6c615d414d3b804cd0263797c7d1f623a
theeam <eduuardomuller@gmail.com>
2018-04-08 18:35:26 -0300

    Section: 2 exercise 2

--- index.js
+++ index.js
@@ -4,13 +4,31 @@ const negativeNumbers = [];
 
 function extractNegativeNumbers(numbers) {
     numbers.forEach(element => {
-        if(element < 0){
+        if (element < 0) {
             negativeNumbers.push(element);
         }
     });
 }
 
-let numbers = [1,2,0,-5,3, -8, -10];
+let numbers = [1, 2, 0, -5, 3, -8, -10];
 
 extractNegativeNumbers(numbers);
 console.log(negativeNumbers);
+
+//Setting a Largest String
+//Implement the funcion, which has a single argument (strings an array of strings) which will set the largestString variable to the string which has the longest length of the array.
+
+let largestString;
+
+function findLargestString(strings) {
+
+    largestString = [];
+    for (const word of strings) {
+        if (largestString.length < word.length) {
+            largestString = word;
+        }
+    }
+}
+
+let strings = ['Eduardo', 'Teste', 'Paralelepipedo', 'No', 'Supercalifragilisticexpialidocious', ''];
+findLargestString(strings);
